d52c75bc30f8ed73951fb77d181af13bbfd96445
Nebula <jsw0325@163.com>
2022-12-09 21:15:27 +0800

    feat: finish

---
+++ src/routes/edit.jsx
@@ -0,0 +1,69 @@
+import { Form, useLoaderData, redirect, useNavigate } from 'react-router-dom'
+import { updateContact } from '../contacts'
+
+export async function action({ request, params }) {
+  const formData = await request.formData()
+  const updates = Object.fromEntries(formData)
+  await updateContact(params.contactId, updates)
+  return redirect(`/contacts/${params.contactId}`)
+}
+
+export default function EditContact() {
+  const contact = useLoaderData
+  const navigate = useNavigate()
+
+  return (
+    <Form method="post" id="contact-form">
+      <p>
+        <span>Name</span>
+        <input
+          type="text"
+          placeholder="First"
+          aria-label="First Name"
+          name="first"
+          defaultValue={contact.first}
+        />
+        <input
+          type="text"
+          placeholder="last"
+          aria-label="Last Name"
+          name="last"
+          defaultValue={contact.last}
+        />
+      </p>
+      <label>
+        <span>Twitter</span>
+        <input
+          type="text"
+          name="twitter"
+          placeholder="@jack"
+          defaultValue={contact.twitter}
+        />
+      </label>
+      <label>
+        <span>Avatar URL</span>
+        <input
+          type="text"
+          name="avatar"
+          placeholder="https://example.com/avatar.jpg"
+          defaultValue={contact.avatar}
+        />
+      </label>
+      <label>
+        <span>Notes</span>
+        <textarea name="notes" defaultValue={contact.notes} rows={6} />
+      </label>
+      <p>
+        <button type="submit">Save</button>
+        <button
+          type="button"
+          onClick={() => {
+            navigate(-1)
+          }}
+        >
+          Cancel
+        </button>
+      </p>
+    </Form>
+  )
+}
